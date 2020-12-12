23302bd0f4bc9a7e550c2cddfca05fe1a27dce70
Lucas Sandeville <lucas.sandeville@tegraincorporadora.com.br>
2020-12-12 13:26:34 -0300

    feat: properly format longer responses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import InstallButton from "./InstallButton";
-import FeedbackIcons from "./FeedbackIcons";
 import "./App.css";
 
 function App() {
@@ -26,32 +25,30 @@ function App() {
     const targetNode = document.querySelector('#WACContainer');
     const config = { attributes: true, childList: true, subtree: true };
     
-    const formatResponseElement = (mutation, cssQuery) => {
-      let watsonResColl = mutation.target.querySelectorAll(cssQuery);
-      let watsonRes = watsonResColl[watsonResColl.length-1]
-      
-      if (watsonRes) {
-        watsonRes.innerHTML = watsonRes.innerHTML.split('\n').map(content=> `<p>${content}</p>`).join(' ')
-        .replace(/&lt;/g, '<').replace(/&gt;/g, '>')
-
-        // edit pdf urls so they can be seen on mobile without needing to download them
-        const pdfViewerUrl = 'https://docs.google.com/viewer?url='
-        const resLinks = watsonRes.querySelectorAll('a');
-        
-        resLinks.forEach(link => {
-          if(link.href.includes('.pdf') && !link.href.includes(pdfViewerUrl)) {
-            link.href = pdfViewerUrl + link.href
-          };
-        })
-      }
-    }
-    
     const formatResponse = function(mutationsList, observer) {
         // Use traditional 'for loops' for IE 11
         for(const mutation of mutationsList) {
           if (mutation.target.id === 'WAC__messages') {
-            formatResponseElement(mutation, '.WAC__searchResult--padding')
-            formatResponseElement(mutation, '.WAC__searchResponseBody')
+            let watsonResColl = mutation.target.querySelectorAll('.WAC__searchResult--padding');
+            let watsonRes = watsonResColl[watsonResColl.length-1]
+            
+            if (watsonRes) {
+              const longRes = watsonRes.querySelector('.WAC__searchResponseBody');
+              if(longRes) watsonRes = longRes;
+
+              watsonRes.innerHTML = watsonRes.innerHTML.split('\n').map(content=> `<p>${content}</p>`).join(' ')
+              .replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+
+              // edit pdf urls so they can be seen on mobile without needing to download them
+              const pdfViewerUrl = 'https://docs.google.com/viewer?url='
+              const resLinks = watsonRes.querySelectorAll('a');
+              
+              resLinks.forEach(link => {
+                if(link.href.includes('.pdf') && !link.href.includes(pdfViewerUrl)) {
+                  link.href = pdfViewerUrl + link.href
+                };
+              })
+            }
           }
         }
     };
