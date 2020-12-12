1c8ce7dbbaa64d00890470a0a76f7b996820cbd8
Lucas Sandeville <lucas.sandeville@tegraincorporadora.com.br>
2020-12-12 01:16:18 -0300

    fix: rollback to previous installable version

--- src/service-worker.js
+++ src/service-worker.js
@@ -63,19 +63,10 @@ registerRoute(
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-// self.addEventListener('message', (event) => {
-//   if (event.data && event.data.type === 'SKIP_WAITING') {
-//     self.skipWaiting();
-//   }
-// });
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      cache.put(event.request, response.clone());
-      return response;
-    }).catch(_ => {
-      return caches.match(event.request);
-    })
-  );
- });
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 // Any other custom service worker logic can go here.
